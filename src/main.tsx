162710954415b1b6410dacaeb4f4c9716838103c
BVK Ratnesh <ratneshbvk2504@gmail.com>
2026-03-07 00:30:52 +0530

    Adding react query options

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,14 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "./index.css";
 import App from "./App.tsx";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 1,
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 
 createRoot(document.getElementById("root")!).render(
   <QueryClientProvider client={queryClient}>
